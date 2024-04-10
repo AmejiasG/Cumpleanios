d274c005e7808f2f2a8890bb73ed1137a590a75c
alemejias <alemejias123@gmail.com>
2024-04-10 02:53:50 -0400

    ahoraSi

--- js/index.js
+++ js/index.js
@@ -5,23 +5,10 @@ function saludar(){
         icon: "success"
       });
 
+      const msg = document.getElementById("disfruta");
+      msg.innerHTML =  `<h2>disfruta :)<h2/> `
+
 }
  
-let btn = document.getElementById("boton");
+const btn = document.getElementById("boton");
 btn.addEventListener("click", saludar)
-
-const seccionesOcultas = document.querySelectorAll('.hidden');
-
-const observer = new IntersectionObserver((entradas) => {
-    entradas.forEach((entry) => {
-
-    entry.target.classList.toggle('mostrar', entry.isIntersecting)
-    });
-
-},
-
-{threshold:0}
-
-);
-
-seccionesOcultas.forEach((seccion) => observer.observe(seccion))
